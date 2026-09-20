5093a24ce7d36a32d0aba4b551fa6370392d7925
Настя <nastygit@mail.ru>
2026-09-20 23:43:47 +0300

    Lab04: if and else, age categories

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// int age = 20;
+// if (age >= 21) {
+//     Console.WriteLine("Доступ разрешён");
+// }
+// Console.WriteLine("Программа продолжает работу");
+
+int age = 15;
+if (age >= 18){
+    Console.WriteLine("Доступ разрешён");
+} else {
+    Console.WriteLine("Доступ запрещён");
+    Console.WriteLine($"До совершеннолетия {18 - age}");
+}
--- obj/Debug/net10.0/Lab04.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e968987cac34f49b61395d6e25194babd4ab778")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
